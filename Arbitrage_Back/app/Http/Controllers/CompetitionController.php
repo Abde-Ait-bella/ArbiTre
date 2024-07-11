b945efd9113034fa4fab0a3ea42d2f225aa6c6f7
Abde-Ait-bella <abdessamadaitbella1998@gmail.com>
2024-07-11 23:51:03 +0100

    push befor settings

--- Arbitrage_Back/app/Http/Controllers/CompetitionController.php
+++ Arbitrage_Back/app/Http/Controllers/CompetitionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\competition;
+use App\Models\Competition;
 use Illuminate\Http\Request;
 
 class CompetitionController extends Controller
