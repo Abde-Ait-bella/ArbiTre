6bf9b454ccca92703ee019b34b68953284368548
Abdessamad Ait-bella <abdessamadaitbella1998@gmail.com>
2025-07-03 03:00:20 +0100

    ...

--- Arbitrage_Front/postcss.config.js
+++ Arbitrage_Front/postcss.config.js
@@ -1,6 +1,6 @@
-  const config = {
-    plugins: {
-      "@tailwindcss/postcss": {},
-    },
-  };
-  export default config;
+// export  const config = {
+//     plugins: {
+//       "@tailwindcss/postcss": {},
+//     },
+//   };
+  // export default config;
